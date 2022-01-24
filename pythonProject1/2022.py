f2f60ca3fa10953b9cc516e6fe18155ae474fc40
artikkitra <dotaplayer9810@gmail.com>
2022-01-24 14:34:41 +0400

    dd

--- pythonProject1/2022.py
+++ pythonProject1/2022.py
@@ -762,7 +762,7 @@ from abc import ABC, abstractmethod
 # print(c4.get_format_time())
 
 # class Student:
-#     def __init__(self,name,marks):
+#     def __init__(self, name, marks):
 #         self.name = name
 #         self.marks = list(marks)
 #
@@ -781,9 +781,196 @@ from abc import ABC, abstractmethod
 #
 #         self.marks[key] = value
 #
+#     def __delitem__(self, key):
+#         if not isinstance(key, int):
+#             raise TypeError('Индекс должен быть целым числом')
+#         del self.marks[key]
 #
-# s1 = Student('Sergey', [5,5,3,4,5])
+#
+# s1 = Student('Sergey', [5, 5, 3, 4, 5])
 # print(s1[0])
-# s1[2] = 4
-# print(s1[2])
+# s1[10] = 4
+# del s1[2]
+# print(s1.marks)
 
+# class Point3D:
+#     CH = 'Координата должна быть числом'
+#     RIGHT = 'Правый операнд должен быть типом Point3D'
+#
+#     def __init__(self, x=0, y=0, z=0):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+#
+#     def __str__(self):
+#         return f'{self.x}, {self.y}, {self.z}'
+#
+#     def __add__(self, other):
+#         if not isinstance(other, Point3D):
+#             raise ValueError(self.RIGHT)
+#         else:
+#             return Point3D(self.__x + other.x, self.__y + other.y, self.__z + other.z)
+#
+#     def __sub__(self, other):
+#         if not isinstance(other, Point3D):
+#             raise ValueError(self.RIGHT)
+#         else:
+#             return Point3D(self.__x - other.x, self.__y - other.y, self.__z - other.z)
+#
+#     def __mul__(self, other):
+#         if not isinstance(other, Point3D):
+#             raise ValueError(self.RIGHT)
+#         else:
+#             return Point3D(self.__x * other.x, self.__y * other.y, self.__z * other.z)
+#
+#     def __truediv__(self, other):
+#         if not isinstance(other, Point3D):
+#             raise ValueError(self.RIGHT)
+#         self.__check0(other)
+#         return Point3D(self.__x / other.x, self.__y / other.y, self.__z / other.z)
+#
+#     def __eq__(self, other):
+#         if not isinstance(other, Point3D):
+#             raise ValueError(self.RIGHT)
+#         return self.__x == other.x and self.__y == other.y == self.__z == other.z
+#
+#     def __getitem__(self, item):
+#         if not isinstance(item, str):
+#             raise TypeError('Ключ должен быть строкой')
+#         elif item == 'x':
+#             return self.__x
+#         elif item == 'y':
+#             return self.__y
+#         elif item == 'z':
+#             return self.__z
+#         else:
+#             print('Неверное значение ключа')
+#
+#     def __setitem__(self, key, value):
+#         if not isinstance(key, str):
+#             raise TypeError('Ключ должен быть строкой')
+#         if self.__check_value(value):
+#             if key == 'x':
+#                 self.__x = value
+#             if key == 'y':
+#                 self.__y = value
+#             if key == 'z':
+#                 self.__z = value
+#         else:
+#             print('Неверное значение ключа')
+#
+#     @staticmethod
+#     def __check0(v):
+#         if v.x == 0 or v.y == 0 or v.z == 0:
+#             raise ZeroDivisionError('Значение правого операнда не должна равнятся 0')
+#
+#     @staticmethod
+#     def __check_value(v):
+#         return isinstance(v, int) or isinstance(v, float)
+#
+#     @property
+#     def x(self):
+#         return self.__x
+#
+#     @x.setter
+#     def x(self, value):
+#         if self.__check_value(value):
+#             self.__x = value
+#         else:
+#             print(self.CH)
+#
+#     @property
+#     def y(self):
+#         return self.__y
+#
+#     @y.setter
+#     def y(self, value):
+#         if self.__check_value(value):
+#             self.__y = value
+#         else:
+#             print(self.CH)
+#
+#     @property
+#     def z(self):
+#         return self.__z
+#
+#     @z.setter
+#     def z(self, value):
+#         if self.__check_value(value):
+#             self.__z = value
+#         else:
+#             print(self.CH)
+#
+#
+# pt = Point3D(12, 15, 18)
+# pt2 = Point3D(6, 3, 9)
+# print(f'Координаты 1-й точки: {pt}')
+# print(f'Координаты 2-й точки: {pt2}')
+# pt3 = pt + pt2
+# print(f'Сложение координат: ({pt3})')
+# pt4 = pt - pt2
+# print(f'Вычитание координат: ({pt4})')
+# pt5 = pt * pt2
+# print(f'Умножение: ({pt5})')
+# pt6 = pt / pt2
+# print(f'Деление: ({pt6})')
+# print(f'Равенство координат: {pt == pt2}')
+#
+# print('x =', pt['x'], 'x1 =', pt2['x'])
+# print('x =', pt['y'], 'x1 =', pt2['y'])
+# print('x =', pt['z'], 'x1 =', pt2['z'])
+#
+# pt['x'] = 20
+# print('Запись значения в координату x:', pt['x'])
+# print(f'Координаты 1-й точки: {pt}')
+
+# class Rectangle:
+#     def __init__(self, w, h):
+#         self.w = w
+#         self.h = h
+#
+#     def get_per(self):
+#         return 2 * (self.w + self.h)
+#
+#
+# class Square:
+#     def __init__(self, a):
+#         self.a = a
+#
+#     def get_per(self):
+#         return 4 * self.a
+#
+#
+# r1 = Rectangle(1, 2)
+# r2 = Rectangle(3, 4)
+# print(r1.get_per(), r2.get_per())
+#
+# s1 = Square(10)
+# s2 = Square(20)
+# print(s1.get_per(), s2.get_per())
+#
+# shape = [r1, r2, s1, s2]
+#
+# for q in shape:
+#     print(q.get_per())
+
+# class Number:
+#     def __init__(self, value):
+#         self.value = value
+#
+#     def total(self, a):
+#         return self.value + int(a)
+#
+#
+# class Text:
+#     def __init__(self, value):
+#         self.value = value
+#
+#     def total(self, a):
+#         return len(self.value + str(a))
+#
+#
+# t1 = Number(10)
+# t2 = Text('Python')
+# print(t1.total(35))
+# print(t2.total(35))
